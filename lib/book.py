6606648626e6ffa8436f95e0b0d054c6d920c428
Priscilla-22 <priscillamw123@gmail.com>
2024-03-03 00:31:05 +0300

    feat:OOP

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 
+
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self._title = title
+        self._page_count = page_count
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, new_title):
+        self._title = new_title
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, new_page_count):
+        if not isinstance(new_page_count, int):
+            print("page_count must be an integer")
+        self._page_count = new_page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,41 @@
 #!/usr/bin/env python3
 
+
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self._brand = brand
+        self._size = size
+        self._condition = "New"
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, new_size):
+        if not isinstance(new_size, int):
+            print("size must be an integer")
+        self._size = new_size
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self._condition = "New"
+
+    @property
+    def condition(self):
+        return self._condition
+
+
+quality_shoe = Shoe("AirMax", 9)
+print(quality_shoe.brand)
+print(quality_shoe.size)
+print(quality_shoe.condition)
+quality_shoe.size = 36
+print(quality_shoe.size)
+
+quality_shoe.cobble()
+print(quality_shoe.condition)
